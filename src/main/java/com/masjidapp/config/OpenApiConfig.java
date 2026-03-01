98b30b033a55329530034e6bb354daee1d829bb7
tahiralamr <YOUR_GITHUB_EMAIL>
2026-03-02 00:56:23 +0530

    refactor cross config

--- src/main/java/com/masjidapp/config/OpenApiConfig.java
+++ src/main/java/com/masjidapp/config/OpenApiConfig.java
@@ -23,7 +23,7 @@ public class OpenApiConfig {
                                 .description("Local Server");
 
                 Server devServer = new Server()
-                                .url("https://3.6.40.125.nip.io")
+                        .url("https://3.6.40.125.nip.io/api/v1")
                                 .description("Dev Server (AWS)");
 
                 return new OpenAPI()
