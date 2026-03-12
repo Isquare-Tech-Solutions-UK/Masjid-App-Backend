46c5a1a74b3fbdecb82f9724f4a8b8ec75c44dae
syed-ga-pf <syed.fardeen+pf@in.geekyants.com>
2026-03-13 00:58:08 +0530

    fix: update nginx domain to new EC2 IP and make Swagger server URL dynamic
    - Updated nginx config from 3.6.40.125.nip.io to 13.126.229.1.nip.io
    - Removed hardcoded server URLs from OpenApiConfig so Swagger auto-detects the current host

--- src/main/java/com/masjidapp/config/OpenApiConfig.java
+++ src/main/java/com/masjidapp/config/OpenApiConfig.java
@@ -4,27 +4,16 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
         @Bean
         public OpenAPI customOpenAPI() {
-                Server localServer = new Server()
-                                .url("http://localhost:8080/api/v1")
-                                .description("Local Server");
-
-                Server devServer = new Server()
-                        .url("https://3.6.40.125.nip.io/api/v1")
-                                .description("Dev Server (AWS)");
-
                 return new OpenAPI()
-                                .servers(List.of(localServer, devServer))
                                 .info(new Info()
                                                 .title("Masjid App API")
                                                 .version("1.0")
